e33d83cbdb5b42d3945a4a97981ee67c46b81bf5
Varvara Borodina <vava.borodina@mail.ru>
2023-07-25 00:46:39 +0300

    feat: set up environment

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+
+import terser from '@rollup/plugin-terser'
+import PeerDepsExternalPlugin from 'rollup-plugin-peer-deps-external'
+import eslint from '@rollup/plugin-eslint'
+
+const packageJson = require('./package.json')
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/cjs/index.js',
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/esm/index.js',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      typescript(),
+      PeerDepsExternalPlugin(),
+
+      resolve(),
+      commonjs(),
+
+      terser(),
+      eslint({}),
+    ],
+  },
+  {
+    input: 'dist/cjs/types/src/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'cjs' }],
+    plugins: [dts.default()],
+  },
+  {
+    input: 'dist/esm/types/src/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts.default()],
+  },
+]
